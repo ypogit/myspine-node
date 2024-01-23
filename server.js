4a7fc2c248e167ed32f9df26c5da0cae157aeabe
lvcy <lvcyferrous@gmail.com>
2024-01-23 09:28:24 -0700

    config: initial config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Building MySpine with Node.js\n');
+    res.end('Hello World\n');
 });
 
 server.listen(3000, () => {
